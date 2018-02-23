0a08896aa6a3b0e1807689cfd5b2c3427bddddbe
eduardo <eduardo27_minotauro@hotmail.com>
2018-02-23 14:59:10 -0300

    download

--- src/javaoit8/Curso.java
+++ src/javaoit8/Curso.java
@@ -1,5 +1,36 @@
 package javaoit8;
 
+/*
+////////////////////////download de arquivo ///////////////////////////////////////////////////////////////////////////////
+
+@Override
+    public ResponseEntity<Resource> downloadCertificateSandboxFile() {
+
+        byte[] certificate = applicationService.findApplicationPlatformConfByPlatform(Platform.IOS)
+                .stream().filter(conf -> conf.getKey().equals(ApplicationPlatformConfKeysTO.APNS_SANDBOX_CERTIFICATE))
+                .map(conf -> conf.getValue())
+                .findFirst().orElse(null);
+
+
+
+        final ByteArrayResource  resource = new ByteArrayResource(certificate);
+
+        final RealwaveContext context = RealwaveContextHolder.getContext();
+        final String fileName = String.format("%s-%s-%s.p12", context.getOrganization(), context.getApplication(), "sandbox");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename="+fileName);
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentLength(resource.contentLength())
+                .contentType(MediaType.parseMediaType("application/x-pkcs12"))
+                .body(resource);
+    }
+
+
+*/
+
 public class Curso {
 	
 	private String nome;
